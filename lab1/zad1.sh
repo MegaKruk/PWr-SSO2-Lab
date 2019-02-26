1eb175104b10caf60e338beb8ecc5645c460fbbd
MegaKruk <filip.mazur.dev@gmail.com>
2019-02-26 18:36:23 +0100

    Update zad1.sh

--- lab1/zad1.sh
+++ lab1/zad1.sh
@@ -6,12 +6,9 @@
 #Przy porównaniu należy ignorować podkatalogi.
 #W wyniku wyświetlić na ekranie listę plików o identycznych
 #nazwach w obu katalogach.
+#Udało mi się rozwiązać zadanie w 1 linijce tak jak chciałem :)
 
 #Filip Mazur 226018
 #16.03.18 11:15
 
 find "$1" "$2" -printf '%P\n' | sort | uniq -d
-
-#Przepraszam bardzo za opóźnienie w dostarczeniu zadania.
-#To już się nie powtórzy. 
-#Udało mi się za to rozwiązać zadanie w 1 linijce tak jak chciałem
